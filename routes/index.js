5fc3cc44cfa4c31eda76d59cef2c590717e1c79e
Kiran Adhikari <kiruu1238@gmail.com>
2021-04-09 13:55:25 +0545

    USE PORT

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { Router } = express;
-const { VerifyCode} = require('../controllers')
+const { VerifyCode, HealthCheck } = require('../controllers')
 const router = Router();
 
 router.post('/verify', VerifyCode)
+router.get('/health', HealthCheck)
 
 
 module.exports = router;
