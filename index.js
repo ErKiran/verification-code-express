d1cd2f5afba5aeb477f59027409aec753e0df307
Kiran Adhikari <kiruu1238@gmail.com>
2021-04-09 13:51:27 +0545

    Heroku Error

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const routes = require('./routes');
+const { HealthCheck } = require('./controllers');
 const app = express();
 
 app.use(cors())
 
 app.use(express.json())
+app.get('/', HealthCheck)
 app.use(routes)
 
+const port = process.env.SERVER_PORT || 5000
 
-app.listen(process.env.SERVER_PORT || 5000,()=>{
+app.listen(port, () => {
     console.log(`App is running on PORT ${process.env.SERVER_PORT}`)
 })
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const { Router } = express;
-const { VerifyCode, HealthCheck } = require('../controllers')
+const { VerifyCode} = require('../controllers')
 const router = Router();
 
 router.post('/verify', VerifyCode)
-router.get('/', HealthCheck)
 
 
 module.exports = router;
